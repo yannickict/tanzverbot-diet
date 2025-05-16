922906dc43f360648e14788d704a16cde840a31b
yannickict <yblatty@edp.ch>
2025-05-16 09:18:34 +0200

    Put the data into an Objects-Array

--- src/tanzverbot-diet.ts
+++ src/tanzverbot-diet.ts
@@ -3,21 +3,64 @@ export enum Sex {
   Female = "f",
 }
 
-const foodNames: string[] = [
-  "Kellogg's Tresor",
-  "Weihenstephan Haltbare Milch",
-  "Mühle Frikadellen",
-  "Volvic Tee",
-  "Neuburger lockerer Sahnepudding",
-  "Lagnese Viennetta",
-  "Schöller 10ForTwo",
-  "Ristorante Pizza Salame",
-  "Schweppes Ginger Ale",
-  "Mini Babybel",
+export interface Food {
+  name: string;
+  calories: number;
+  servings: number;
+}
+
+const foodData: Food[] = [
+  {
+    name: "Kellogg's Tresor",
+    calories: 137,
+    servings: 4,
+  },
+  {
+    name: "Weihenstephan Haltbare Milch",
+    calories: 64,
+    servings: 8,
+  },
+  {
+    name: "Mühle Frikadellen",
+    calories: 271,
+    servings: 4,
+  },
+  {
+    name: "Volvic Tee",
+    calories: 40,
+    servings: 12,
+  },
+  {
+    name: "Neuburger lockerer Sahnepudding",
+    calories: 297,
+    servings: 1,
+  },
+  {
+    name: "Lagnese Viennetta",
+    calories: 125,
+    servings: 6,
+  },
+  {
+    name: "Schöller 10ForTwo",
+    calories: 482,
+    servings: 2,
+  },
+  {
+    name: "Ristorante Pizza Salame",
+    calories: 835,
+    servings: 2,
+  },
+  {
+    name: "Schweppes Ginger Ale",
+    calories: 37,
+    servings: 25,
+  },
+  {
+    name: "Mini Babybel",
+    calories: 59,
+    servings: 20,
+  },
 ];
-const foodCalories: number[] = [137, 64, 271, 40, 297, 125, 482, 835, 37, 59];
-const foodServings: number[] = [4, 8, 4, 12, 1, 6, 2, 2, 25, 20]; 
-//todo: kann als Objekt gespeichert werden
 
 export function calcDateOnDiet(
   currentWeightKg: number,
@@ -34,9 +77,9 @@ export function calcDateOnDiet(
     throw new Error(`You do not qualify for this kind of diet.`);
   }
   let dailyCaloriesOnDiet = 0;
-  for (const index in foodNames) {
-    const calories = foodCalories[index] || 0;
-    const servings = foodServings[index] || 0;
+  for (const index in foodData) {
+    const calories = foodData[index].calories || 0;
+    const servings = foodData[index].servings || 0;
     dailyCaloriesOnDiet += calories * servings;
   }
   //todo: Erstelltes Objekt nachher nehmen
